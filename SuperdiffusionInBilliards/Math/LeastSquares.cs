2538f08d78c67ae30740a491c3e26fce6d17c29e
Sasha <Sasha>
2016-11-24 00:49:04 +0300

    Класс МНК переделан из static в обычный. Убран один файл

--- SuperdiffusionInBilliards/Math/LeastSquares.cs
+++ SuperdiffusionInBilliards/Math/LeastSquares.cs
@@ -5,47 +5,40 @@ using System.Text;
 
 namespace SuperdiffusionInBilliards
 {
-    static public class LeastSquares
+    public class LeastSquares
     {
-        //static public List<double> ShiftAndSlope(List<Point2D> pointsForApprox)
-        static public List<double> ShiftAndSlope(List<Point2D> pointsForApprox)
+        private double slope, shift;
+        private double sumX = 0;
+        private double sumX2 = 0;
+        private double sumY = 0;
+        private double sumXY = 0;
+        private int numOfPoints = 0;
+    
+        public LeastSquares(List<Point2D> pointsForApprox)
         {
-            List<double> coefficients = new List<double>();
-            double a, b;
-            double sumX = 0;
-            double sumX2 = 0;
-            double sumY = 0;
-            double sumXY = 0;
-            int numOfPoints = 0;
+            numOfPoints = pointsForApprox.Count;
             foreach (Point2D point in pointsForApprox)
             {
-                numOfPoints += 1;
                 sumX += point.X;
                 sumY += point.Y;
-                sumX2 += (point.X*point.X);
-                sumXY += (point.X * point.Y);
-            }
-            a = (numOfPoints * sumXY - sumY)/(numOfPoints * sumX2 - sumX);
-            b = (sumY - a * sumX) / numOfPoints;
-            coefficients.Add(a);
-            coefficients.Add(b);
-            return coefficients;
-        }
-        
-        static public double Slope(List<Point2D> pointsForApprox, double b)
-        {
-            double slope;
-            double sumX = 0;
-            double sumX2 = 0;
-            double sumXY = 0;
-            foreach (Point2D point in pointsForApprox)
-            {
-                sumX += point.X;
                 sumX2 += (point.X * point.X);
                 sumXY += (point.X * point.Y);
             }
-            slope = (sumXY - b * sumX) / sumX2;
-            return 0;
+        }
+        public List<double> ShiftAndSlope()
+        {
+            List<double> coefficients = new List<double>();
+            slope = (numOfPoints * sumXY - sumY)/(numOfPoints * sumX2 - sumX);
+            shift = (sumY - slope * sumX) / numOfPoints;
+            coefficients.Add(slope);
+            coefficients.Add(shift);
+            return coefficients;
+        }
+        
+        public double Slope(double b)
+        {
+           slope = (sumXY - b * sumX) / sumX2;
+           return slope;
         }
     }
 }
