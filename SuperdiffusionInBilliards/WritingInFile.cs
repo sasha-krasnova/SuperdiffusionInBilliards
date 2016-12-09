40899eac6a90da22bf9f12f69e218bfee3db233a
sasha-krasnova <sasha_krasnova@yahoo.com>
2016-12-09 14:28:21 +0300

    Начата работа над классом для записи в файл

---
+++ SuperdiffusionInBilliards/WritingInFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperdiffusionInBilliards
+{
+    class WritingInFile
+    {
+        List<Graph> graphs = new List<Graph>();
+
+
+    }
+}
